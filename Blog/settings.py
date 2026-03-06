1d82eeaf6bddd044d517f559f5af3c84f032678d
Dineshrajvarma143 <penungondadineshrajvarma@gmail.com>
2026-03-06 16:32:18 +0530

    Mysql to SQllite

--- Blog/settings.py
+++ Blog/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
-        'USER': 'root',
-        'PASSWORD': 'Dinesh@143',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
